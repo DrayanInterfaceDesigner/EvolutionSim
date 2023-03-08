2b29355bac6d262984e69c5465646324271a0ef8
Drayan Silva Magalhães <31392046+DrayanInterfaceDesigner@users.noreply.github.com>
2023-03-08 06:16:44 -0300

    comments

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ entity = Entity.Entity()
 
 if __name__ == "__main__":
     canvas = Canvas(window,height=500, width=500)
+    # ---- START ----
+    # everything you want to put inside the world
+    # goes below this line
+
     entity.update(canvas)
 
+    # ---- END ----
     canvas.pack()
     window.mainloop()
--- scripts/Entity.py
+++ scripts/Entity.py
@@ -1,15 +1,21 @@
 class Entity:
     def __init__(self) -> None:
+        # position in cartesian stage
         self.pos = {"x": 0, "y": 0}
+        # scale, the size of the entity scales with it
         self.scale = float(1.0)
+        # the base size to scale by self.scale
         self.base_size = 10
+        # the size to be used in calculations
         self.size = {"width": self.base_size * self.scale, 
                      "height": self.base_size * self.scale}
         self.color = "#000000"
     
-
+    # everything inside here updates every frame
     def update(self, canvas):
-        self.draw(canvas)
-    def draw(self, canvas):
+        self.render(canvas)
+        
+    # to draw the entity
+    def render(self, canvas):
         canvas.create_rectangle(self.size["width"], self.size["height"],
                                  2,2, fill=self.color)
